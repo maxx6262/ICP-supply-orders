f81f5dfc4a751eaa7dd38c046024bf17e7d6ecf8
LECOUSTRE Maxime <MLECOUSTRE@nexity.fr>
2023-08-11 15:43:37 +0200

    fix issue of typ fix dfx config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
+// First of all, we manage products & items supplied and orderable
+
 /**
  * This type represents an asset that can be bought or put on sale
  */
@@ -39,4 +41,15 @@ export function addItem(payload: ItemPayload) {
     const item: Item = { id: uuidv4(), firstReferenceAt: ic.time(), stockEstimation: Opt.None, disabled: false, updatedAt: Opt.None, ...payload };
     common_catalog.insert(item.id, item);
     return Result.Ok(item);
+}
+
+// a workaround to make uuid package work with Azle
+globalThis.crypto = {
+    getRandomValues: () => {
+        let array = new Uint8Array(32);
+        for(let i = 0; i < array.length; i++) {
+            array[i] = Math.floor(Math.random() * 256);
+        }
+        return array;
+    }
 }
